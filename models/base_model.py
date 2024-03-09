a145effc2dad8c1788874d070e7e2d3a0e2fe91d
Itohan1 <momoduitohan5@gmail.com>
2024-03-09 10:57:37 -0800

    duress

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Write a class BaseModel"""
 
 from models.__init__ import storage
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python3
+""" Module to test all the model files. """
 
+
+class TestBaseModel(unittest.TestCase):
+    def test_model(self):
+        pass
