f147af9fd7d9a9e270b824495611ffd0bd166295
Gift Aghaulor <aghaulor.gift@gmail.com>
2024-03-10 17:15:24 +0100

    update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,10 @@
-#!/usr/bin/python3
 """ Module that test all class, function and instances of the BaseModel"""
-
 
 import unittest
 from models.base_model import BaseModel
-from models.__init__ import storage
 from datetime import datetime
 import os
-
+import uuid
 
 class TestBaseModel(unittest.TestCase):
     """ Test cases for the BaseModel"""
@@ -38,7 +35,8 @@ class TestBaseModel(unittest.TestCase):
         
     def test_save(self):
         """ Test the saveing function of the BaseModel"""
-
+        
+        base_model = BaseModel()
         old_updated_at = self.model.updated_at
         self.model.save()
         self.assertNotEqual(self.model.updated_at, old_updated_at)
@@ -46,6 +44,7 @@ class TestBaseModel(unittest.TestCase):
     
     def test_to_dict(self):
         """ Test the conversion to dictionary object"""
+        base_model = BaseModel()
 
         instance_dict = self.model.to_dict()
         self.assertIn('__class__', instance_dict)
@@ -59,3 +58,14 @@ class TestBaseModel(unittest.TestCase):
 
         self.assertIsInstance(storage.all(), dict)
         self.assertIn(f"BaseModel.{self.model.id}", storage.all())
+
+    def test_uuid_format(self):
+        """ Test that id is a valid UUID"""
+        base_model = BaseModel()
+        try:
+            uuid.UUID(obj.id, version=4)
+        except ValueError:
+            self.fail("Invalid UUID format for id")
+
+if __name__== '__main__':
+    unittest.main()
