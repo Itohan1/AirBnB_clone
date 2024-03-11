71a6d2f341f60b1b92da937703cfa64a77625bc3
Gift <aghaulor.gift@gmail.com>
2024-03-11 07:47:00 -0700

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class that inherit the state from the BaseModel"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class that inherits from BaseModel"""
+    
+    def __init__(self, *args, **kwargs):
+        """Initialize State instance"""
+
+        super().__init__(*args, **kwargs)
+        self.name = kwargs.get('name', '')
