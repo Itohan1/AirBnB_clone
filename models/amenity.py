0cb424311828a36d991c0c3788552f60cf116636
Gift <aghaulor.gift@gmail.com>
2024-03-11 07:48:06 -0700

    Create amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A class amenity that inherits from the BaseModel"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class that inherits from BaseModel"""
+    
+    def __init__(self, *args, **kwargs):
+        """Initialize Amenity instance"""
+
+        super().__init__(*args, **kwargs)
+        self.name = kwargs.get('name', '')
