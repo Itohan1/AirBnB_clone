890cd43bd38f79b09220b176bacf5df09e0f5bd2
Gift <aghaulor.gift@gmail.com>
2024-03-11 04:42:06 -0700

    Fix NameError in the base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,11 @@ class BaseModel:
                 elif key == 'created_at' or key == 'updated_at':
                     if isinstance(value, datetime):
                         value = value.isoformat()
-                        Self.__dict__[key] = datetime.strptime(
+                        self.__dict__[key] = datetime.strptime(
                                 value, '%Y-%m-%dT%H:%M:%S.%f')
                     
                 else:
-                    Self.__dict__[key] = value
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
