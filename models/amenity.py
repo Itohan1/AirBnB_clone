0d167cf9e304285e44a94bac4f7a9a28c0c60c59
Gift <aghaulor.gift@gmail.com>
2024-03-11 08:02:34 -0700

    Fix pycodestyle error

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """Amenity class that inherits from BaseModel"""
-    
+
     def __init__(self, *args, **kwargs):
         """Initialize Amenity instance"""
 
