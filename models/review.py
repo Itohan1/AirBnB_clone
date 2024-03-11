9828634a4df9c719cdf1ec7f67d884194f2e15e6
Gift <aghaulor.gift@gmail.com>
2024-03-11 08:08:43 -0700

    Fix pycodestyle error

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """Review class that inherits from BaseModel"""
-    
+
     def __init__(self, *args, **kwargs):
         """Initialize Review instance"""
 
